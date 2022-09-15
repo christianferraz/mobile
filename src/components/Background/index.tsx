1f8dbf70900e2228c9634bda726520e62dc67866
Christian Pacheco <christianferraz@gmail.com>
2022-09-15 01:25:21 -0400

    First commit

---
+++ src/components/Background/index.tsx
@@ -0,0 +1,16 @@
+import { ImageBackground } from 'react-native';
+
+import { styles } from './styles';
+import backGroundImg from '../../assets/background-galaxy.png'
+
+interface Props {
+  children: React.ReactNode
+}
+
+export function Background({ children }: Props) {
+  return (
+    <ImageBackground source={backGroundImg} defaultSource={backGroundImg} style={styles.container}>
+      {children}
+    </ImageBackground>
+  );
+}
